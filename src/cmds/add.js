3572b16147cd6c9381f5e6daafadaecff3380611
cywang22 <cywang22@iflytek.com>
2021-12-06 12:56:01 +0800

    fix: // add.js

--- src/cmds/add.js
+++ src/cmds/add.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-06 09:53:06
- * @LastEditTime: 2021-12-06 11:19:48
+ * @LastEditTime: 2021-12-06 12:55:34
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \geek-todo\src\cmds\add.js
@@ -10,7 +10,7 @@ const { v4: uuid } = require('uuid')
 const { createdPrompt } = require('../util/print')
 
 module.exports =  function(args, db) {
-  console.log(args)
+  // console.log(args)
   const todoItem = args._.slice(1).join(' ')
   if (todoItem) {
     db.run(`
