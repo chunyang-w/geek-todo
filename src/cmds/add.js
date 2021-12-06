0a9bff5232272cf8a4252c774cc625abe1835930
cywang22 <cywang22@iflytek.com>
2021-12-06 11:21:01 +0800

    fix: fix dastabase init, add subcommand split world issue

--- src/cmds/add.js
+++ src/cmds/add.js
@@ -1,8 +1,17 @@
+/*
+ * @Author: your name
+ * @Date: 2021-12-06 09:53:06
+ * @LastEditTime: 2021-12-06 11:19:48
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \geek-todo\src\cmds\add.js
+ */
 const { v4: uuid } = require('uuid')
 const { createdPrompt } = require('../util/print')
 
 module.exports =  function(args, db) {
-  const todoItem = args._[1]
+  console.log(args)
+  const todoItem = args._.slice(1).join(' ')
   if (todoItem) {
     db.run(`
     INSERT INTO todo_items (id, name, completed, created)
--- src/init.js
+++ src/init.js
@@ -1,16 +1,27 @@
+/*
+ * @Author: your name
+ * @Date: 2021-12-06 09:53:06
+ * @LastEditTime: 2021-12-06 11:15:15
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \geek-todo\src\init.js
+ */
 const fs = require('fs')
 const path = require('path')
 const { initPrompt, initComplete } = require('../src/util/print')
 const { drawLogo, welcome } = require('../src/util/draw')
-// const sqlite3 = require('sqlite3')
+const sqlite3 = require('sqlite3')
 
-module.exports = function init(databasePath, db) {
+function init() {
   // console.log(databasePath)
-  if (!fs.existsSync(databasePath)) {
+  let db
+  const databaseFolder = path.resolve(__dirname ,'../database')
+  const databasePath = path.resolve(__dirname ,'../database/todo.db')
+  if (!fs.existsSync(databaseFolder)) {
     console.log(initPrompt)
-    console.log(initComplete)
-    welcome()
-    // drawLogo()
+    fs.mkdirSync(databaseFolder ,{ recursive: false })
+    fs.writeFileSync(databasePath, '')
+    db = new sqlite3.Database(databasePath)
     db.serialize(() => {
       db.run( 'CREATE TABLE IF NOT EXISTS todo_items (\
                 id VARCHAR(36),\
@@ -19,7 +30,16 @@ module.exports = function init(databasePath, db) {
                 created DATETIME\
               )'
       )
-      db.close()
     })
+    console.log(initComplete)
+    welcome()
+    // drawLogo()
+  } else {
+    db = new sqlite3.Database(databasePath)
   }
+  return db
+}
+
+module.exports = {
+  init
 }
